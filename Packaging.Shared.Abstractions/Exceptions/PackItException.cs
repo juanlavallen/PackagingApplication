06898df9c4b2d46d65e63052e35fad3039a3d1a4
juanlavallen <juanlavallen02@gmail.com>
2023-01-31 23:39:06 -0300

    PackItException

---
+++ Packaging.Shared.Abstractions/Exceptions/PackItException.cs
@@ -0,0 +1,9 @@
+namespace Packaging.Shared.Abstractions.Exceptions
+{
+    public abstract class PackItException : Exception
+    {
+        protected PackItException(string message) : base(message)
+        {
+        }
+    }
+}
