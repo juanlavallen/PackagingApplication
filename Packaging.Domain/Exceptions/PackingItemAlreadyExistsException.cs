6509e57859bad44bce86ae2a31113b84c95dc4c1
juanlavallen <juanlavallen02@gmail.com>
2023-01-31 23:50:25 -0300

    PackingItemAlreadyExistsException

---
+++ Packaging.Domain/Exceptions/PackingItemAlreadyExistsException.cs
@@ -0,0 +1,17 @@
+using Packaging.Shared.Abstractions.Exceptions;
+
+namespace Packaging.Domain.Exceptions
+{
+    public class PackingItemAlreadyExistsException : PackItException
+    {
+        public string ListName { get; }
+        public string ItemName { get; }
+
+        public PackingItemAlreadyExistsException(string listName, string itemName)
+            : base($"Packing List: '{listName}' already defined item '{itemName}'")
+        {
+            ListName = listName;
+            ItemName = itemName;
+        }
+    }
+}
