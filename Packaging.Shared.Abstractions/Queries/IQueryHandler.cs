2c16c37a39c38db10440a3b4e9b84b712fcc1020
juanlavallen <juanlavallen02@gmail.com>
2023-02-08 22:31:09 -0300

    QueryHandler interface

---
+++ Packaging.Shared.Abstractions/Queries/IQueryHandler.cs
@@ -0,0 +1,7 @@
+namespace Packaging.Shared.Abstractions.Queries
+{
+    public interface IQueryHandler<in TQuery, TResult> where TQuery : class, IQuery<TResult>
+    {
+        Task<TResult> HandleAsync(TQuery query);
+    }
+}
