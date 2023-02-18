4d471632174ce0eb48f1454d288e90103fda76d0
juanlavallen <juanlavallen02@gmail.com>
2023-02-17 21:52:40 -0300

    Command Dispatcher interface

---
+++ Packaging.Shared.Abstractions/Commands/ICommandDispatcher.cs
@@ -0,0 +1,7 @@
+namespace Packaging.Shared.Abstractions.Commands
+{
+    public interface ICommandDispatcher
+    {
+        Task DispatchAsync<TCommand>(TCommand command) where TCommand : class, ICommand;
+    }
+}
